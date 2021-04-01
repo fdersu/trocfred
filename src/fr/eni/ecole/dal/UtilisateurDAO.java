97c2466bc4d92f9fe6d915b0cb4cb5a11e0ded46
caoson-eni <caoson167@gmail.com>
2021-04-01 11:16:48 +0200

    Ajouter des méthodes de la couche DAO pour Utilisateur

--- src/fr/eni/ecole/dal/UtilisateurDAO.java
+++ src/fr/eni/ecole/dal/UtilisateurDAO.java
@@ -1,9 +1,8 @@
 package fr.eni.ecole.dal;
 
 import fr.eni.ecole.bo.Utilisateur;
+import fr.eni.ecole.exception.BusinessException;
 
 public interface UtilisateurDAO extends CRUD<Utilisateur> {
-	
-
-
+	Utilisateur find (String pseudo, String motDePasse) throws BusinessException;
 }
